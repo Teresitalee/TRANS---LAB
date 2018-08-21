af3c13ca029216943e9c015ac7e30466d4bef3bd
TeresitaLee <teresita.1852@hotmail.com>
2018-08-21 13:43:58 -0300

    impresion de datos api

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,16 +6,19 @@ function saldoTotal(){
     fetch('http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=123456')
     .then(res => res.json())
     .then (data => {
-        console.log(data.id['2'].saldoTarjeta)
+        console.log(data.id)
+        console.log(data.fechaSaldo)
+        console.log(data.estadoContrato)
+        console.log(data.saldoTarjeta)
         
 
         showBip.innerHTML = `
         <div class="container">
         <div class="row mostrarSaldo ">
         <div class="col-sm-12 text-center my-5">
-        <p class="titulo ml-1" > Saldo Contable  $ </p>
-        <p class="titulo" > Estado Contrato:</p>
-        <p class="titulo" > Fecha Saldo: </p>
+        <p class="titulo ml-1">${data.saldoTarjeta}</p>
+        <p class="titulo" >${data.estadoContrato}</p>
+        <p class="titulo" >${data.fechaSaldo} </p>
         </div>
         </div>
         </div>
